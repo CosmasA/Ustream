196635ddca9f012fb3c08a983ff676712dc72055
BongominErickJuma <ericbongomin@gmail.com>
2024-08-02 19:57:50 +0300

    first commit

---
+++ assets/js/videos.js
@@ -0,0 +1,383 @@
+const videos = [
+  {
+    id: 1,
+    name: "the quick brown fox jumped over the lazy dogs",
+    category: "Comedy",
+    description: "manipsum dolor sit amet, consectetur",
+    videoSrc: "https://www.youtube.com/watch.com/video",
+    chapters: [
+      {
+        id: 1,
+        title:
+          "the first chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-1.png",
+      },
+      {
+        id: 2,
+        title:
+          "the second chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-2.png",
+      },
+      {
+        id: 3,
+        title:
+          "the third chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-3.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-4.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-5.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "the quick brown fox jumped over the lazy dogs",
+    category: "Comedy",
+    description: "manipsum dolor sit amet, consectetur",
+    videoSrc: "https://www.youtube.com/watch.com/video",
+    chapters: [
+      {
+        id: 1,
+        title:
+          "the first chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-1.png",
+      },
+      {
+        id: 2,
+        title:
+          "the second chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-2.png",
+      },
+      {
+        id: 3,
+        title:
+          "the third chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-3.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-4.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-5.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "the quick brown fox jumped over the lazy dogs",
+    category: "Comedy",
+    description: "manipsum dolor sit amet, consectetur",
+    videoSrc: "https://www.youtube.com/watch.com/video",
+    chapters: [
+      {
+        id: 1,
+        title:
+          "the first chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-1.png",
+      },
+      {
+        id: 2,
+        title:
+          "the second chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-2.png",
+      },
+      {
+        id: 3,
+        title:
+          "the third chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-3.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-4.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-5.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "the quick brown fox jumped over the lazy dogs",
+    category: "Comedy",
+    description: "manipsum dolor sit amet, consectetur",
+    videoSrc: "https://www.youtube.com/watch.com/video",
+    chapters: [
+      {
+        id: 1,
+        title:
+          "the first chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-1.png",
+      },
+      {
+        id: 2,
+        title:
+          "the second chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-2.png",
+      },
+      {
+        id: 3,
+        title:
+          "the third chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-3.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-4.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-5.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "the quick brown fox jumped over the lazy dogs",
+    category: "Comedy",
+    description: "manipsum dolor sit amet, consectetur",
+    videoSrc: "https://www.youtube.com/watch.com/video",
+    chapters: [
+      {
+        id: 1,
+        title:
+          "the first chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-1.png",
+      },
+      {
+        id: 2,
+        title:
+          "the second chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-2.png",
+      },
+      {
+        id: 3,
+        title:
+          "the third chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-3.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-4.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-5.png",
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "the quick brown fox jumped over the lazy dogs",
+    category: "Comedy",
+    description: "manipsum dolor sit amet, consectetur",
+    videoSrc: "https://www.youtube.com/watch.com/video",
+    chapters: [
+      {
+        id: 1,
+        title:
+          "the first chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-1.png",
+      },
+      {
+        id: 2,
+        title:
+          "the second chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-2.png",
+      },
+      {
+        id: 3,
+        title:
+          "the third chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-3.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-4.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-5.png",
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: "the quick brown fox jumped over the lazy dogs",
+    category: "Comedy",
+    description: "manipsum dolor sit amet, consectetur",
+    videoSrc: "https://www.youtube.com/watch.com/video",
+    chapters: [
+      {
+        id: 1,
+        title:
+          "the first chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-1.png",
+      },
+      {
+        id: 2,
+        title:
+          "the second chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-2.png",
+      },
+      {
+        id: 3,
+        title:
+          "the third chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-3.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-4.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-5.png",
+      },
+    ],
+  },
+
+  {
+    id: 8,
+    name: "the quick brown fox jumped over the lazy dogs",
+    category: "Comedy",
+    description: "manipsum dolor sit amet, consectetur",
+    videoSrc: "https://www.youtube.com/watch.com/video",
+    chapters: [
+      {
+        id: 1,
+        title:
+          "the first chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-1.png",
+      },
+      {
+        id: 2,
+        title:
+          "the second chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-2.png",
+      },
+      {
+        id: 3,
+        title:
+          "the third chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-3.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-4.png",
+      },
+      {
+        id: 4,
+        title:
+          "the forth chapter of the quick brown fox jumped over the lazy dogs",
+        thumbnailUrl: "/assets/img/tab-5.png",
+      },
+    ],
+  },
+];
+
+function populateVideos(items, elementId) {
+  const element = document.getElementById(elementId);
+  element.innerHTML = "";
+
+  let section = "";
+  items.forEach((item) => {
+    section += `
+    <section id="clients" class="clients section mt-5">
+        <div class="container">
+            <h4 class="mb-2">Socials</h4>
+            <div class="swiper init-swiper">
+                ${swiperConfig}
+                <div class="swiper-wrapper align-items-center">
+                    <div class="swiper-slide">
+                        <div class="swiper-item rounded">
+                            <i class="bi bi-camera-video"></i>
+                            <h3><a href="" class="stretched-link">Video Item</a></h3>
+                        </div>
+                        <p class="text-center">
+                            ${item.name}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    `;
+  });
+}
+
+const swiperConfig = `
+<script type="application/json" class="swiper-config">
+{
+  "loop": true,
+  "speed": 600,
+  "autoplay": {
+    "delay": 5000
+  },
+  "slidesPerView": "auto",
+  "pagination": {
+    "el": ".swiper-pagination",
+    "type": "bullets",
+    "clickable": true
+  },
+  "breakpoints": {
+    "320": {
+      "slidesPerView": 2,
+      "spaceBetween": 10
+    },
+    "480": {
+      "slidesPerView": 3,
+      "spaceBetween": 10
+    },
+    "640": {
+      "slidesPerView": 4,
+      "spaceBetween": 10
+    },
+    "992": {
+      "slidesPerView": 6,
+      "spaceBetween": 10
+    }
+  }
+}
+</script>
+`;
+
+populateVideos(videos, "videos");
