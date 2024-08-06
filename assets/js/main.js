b93847407ebf533435d017b4bd0f49a897c29d5f
BongominErickJuma <ericbongomin>
2024-08-06 16:03:20 +0300

    daily commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,29 @@
   "use strict";
 
   /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    if (all) {
+      select(el, all).forEach((e) => e.addEventListener(type, listener));
+    } else {
+      select(el, all).addEventListener(type, listener);
+    }
+  };
+
+  /**
    * Apply .scrolled class to the body as the page is scrolled down
    */
   function toggleScrolled() {
@@ -28,6 +51,25 @@
       }
     });
   });
+
+  /**
+   * Sidebar toggle
+   */
+  if (select(".toggle-sidebar-btn")) {
+    on("click", ".toggle-sidebar-btn", function (e) {
+      select("body").classList.toggle("toggle-sidebar");
+      console.log(clicked);
+    });
+  }
+
+  /**
+   * Search bar toggle
+   */
+  if (select(".search-bar-toggle")) {
+    on("click", ".search-bar-toggle", function (e) {
+      select(".search-bar").classList.toggle("search-bar-show");
+    });
+  }
 
   /**
    * Scroll top button
